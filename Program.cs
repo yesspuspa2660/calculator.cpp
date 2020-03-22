58522b6299c9fa6c2a20a729764721fe0e68b2d4
yesspuspa2660 <rahayu.2660@students.amikom.ac.id>
2020-03-22 15:07:26 +0700

    class dan object

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tugasclassdanobject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Taxi taxi = new Taxi();
+
+            taxi.DriverName = "jono";
+            taxi.OnDuty = true;
+            taxi.NumPasangger = 10;
+
+
+            taxi.TaxiInfo();
+            Console.WriteLine();
+            taxi.PickUpPasangger();
+            taxi.DropOffPasangger();
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Taxi.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tugasclassdanobject
+{
+    class Taxi
+    {
+        public string DriverName { get; set; }
+        public bool OnDuty { get; set; }
+        public int NumPasangger { get; set; }
+
+        public void TaxiInfo()
+        {
+            Console.WriteLine("Driver Name : {0}", DriverName);
+            Console.WriteLine("OnDuty : {0}", OnDuty);
+            Console.WriteLine("Num Pasangger : {0}", NumPasangger);
+        }
+
+        public void PickUpPasangger()
+        {
+            Console.WriteLine("{0} Sedang Menjemput Penumpang", DriverName);
+        }
+
+        public void DropOffPasangger()
+        {
+            Console.WriteLine("{0} Selesai Mengantar Penumpang", DriverName);
+        }
+    }
+}
