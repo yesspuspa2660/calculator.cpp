f896bfa1dcd88d0c4739a69b967f3c3a5fc6fc42
yesspuspa2660 <rahayu.2660@students.amikom.ac.id>
2020-04-22 22:56:39 +0700

    responsi pemprograman

---
+++ Karyawan.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResponsiPemprograman2660
+{
+    class Karyawan
+    {
+        public string Nama;
+        public int NIK;
+        public int GajiBulanan;
+        public Karyawan(string arg0, int arg1, int arg2)
+        {
+            Nama = arg0;
+            NIK = arg1;
+            GajiBulanan = arg2;
+
+            if (GajiBulanan < 0)
+            {
+                GajiBulanan = 0;
+            }
+        }
+       
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResponsiPemprograman2660
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Karyawan karyawan = new Karyawan("190302123", "kiki", 2000000);
+            Karyawan karyawan2 = new Karyawan("190302124", "iqro", 3500000);
+         
+
+            Console.WriteLine("No. \t\t NIK \t\t GajiBulanan");
+            Console.WriteLine("----------------------------");
+            Console.WriteLine("1. {0} \t {1} \t {2}");
+            Console.WriteLine("2. {0} \t {1} \t {2}");
+            Console.WriteLine("3. {0} \t {1} \t {3}");
+
+            Console.WriteLine("\n\n Yeayyyyy kenaikan gaji 10%!!");
+
+            Console.WriteLine("No. \t\t NIK \t\t Nama \t\t GajiBulanan");
+            Console.WriteLine("--------------------------------------");
+            Console.WriteLine("1. {0} \t {1} \t {2}");
+            Console.WriteLine("2. {0} \t {1} \t {2}");
+            Console.WriteLine("3. {0} \t {1} \t {3}");
+
+            Console.ReadKey();
+
+        }
+    }
+}
